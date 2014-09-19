7acb031f11cc35e0705818be9919a157a431d952
Timothy Kendall <timkendall18@gmail.com>
2014-09-19 15:38:20 -0700

    Added routing and mock data

--- app/scripts/services/catalog.js
+++ app/scripts/services/catalog.js
@@ -5,7 +5,7 @@ angular.module('schedules').factory('Catalog', [
 
 
       return {
-        2014Catalog: [
+        '2014': [
           { id: 'CPSC-353-01', course: 'CPSC-353', section: '1', name: 'Computer Networks & Administration', professorName: 'Michael Fahy', professorId: '334', totalSpots: 30, availableSpots: 12, waitlist: 0 }
         ]
       }
