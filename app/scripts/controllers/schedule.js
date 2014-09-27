1c1f7a4bbc56333d844e0621c0b2ac43d7cac88c
Timothy Kendall <timkendall18@gmail.com>
2014-09-26 20:48:06 -0700

    Finished calendar

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -18,8 +18,48 @@ angular.module('schedules')
       if (!course.picked && User.schedule.credits <= 18) {
          User.addCourse(course);
          course.picked = true;
+
+         switch (course.meets) {
+          case 'M':
+            $scope.mondayCourses.push(course);
+            break;
+          case 'T':
+            $scope.tuesdayCourses.push(course);
+            break;
+          case 'W':
+            $scope.wednesdayCourses.push(course);
+            break;
+          case 'Th':
+            $scope.thursdayCourses.push(course);
+            break;
+          case 'F':
+            $scope.fridayCourses.push(course);
+            break;
+          case 'S':
+            $scope.saturdayCourses.push(course);
+            break;
+          case 'MWF':
+            $scope.mondayCourses.push(course);
+            $scope.wednesdayCourses.push(course);
+            $scope.fridayCourses.push(course);
+            break;
+          case 'TTh':
+            $scope.tuesdayCourses.push(course);
+            $scope.thursdayCourses.push(course);
+            break;
+          default:
+            break;
+         }
       }
     }
+
+    // Containers for calendar
+    $scope.mondayCourses = [];
+    $scope.tuesdayCourses = [];
+    $scope.wednesdayCourses = [];
+    $scope.thursdayCourses = [];
+    $scope.fridayCourses = [];
+    $scope.saturdayCourses = [];
 
     // Pluralization templates
     $scope.courseMorningForms = {
